4f56a6cf5b870a58558eee9c364ee1081709b2ea
naporin0624 <naporin0624@gmail.com>
2024-04-12 23:32:28 +0000

    chore: format

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,21 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import dts from "vite-plugin-dts";
+import { copyFile } from "node:fs/promises";
+import { peerDependencies } from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/index.tsx",
-      name: "index",
-      fileName: "index",
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: ["react"],
-      treeshake: true,
-    }
-  },
-  plugins: [react(), vanillaExtractPlugin(), dts({ rollupTypes: true })],
-})
+	build: {
+		lib: {
+			entry: {
+				index: "src/index.ts",
+			},
+			formats: ["es", "cjs"],
+		},
+		rollupOptions: {
+			external: Object.keys(peerDependencies ?? {}),
+			treeshake: "recommended",
+		},
+		cssCodeSplit: true,
+		sourcemap: true,
+		minify: true,
+		cssMinify: true,
+	},
+	plugins: [
+		react(),
+		vanillaExtractPlugin(),
+		dts({
+			rollupTypes: true,
+			// https://github.com/qmhc/vite-plugin-dts/issues/267
+			afterBuild() {
+				return copyFile("dist/index.d.ts", "dist/index.d.cts");
+			},
+		}),
+	],
+});
