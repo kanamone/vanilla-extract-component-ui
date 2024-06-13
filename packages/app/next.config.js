217e690f5b7927500bde34bc44c30f6b1f91f7bf
naporin0624 <naporin0624@gmail.com>
2024-06-13 00:02:59 +0000

    chore: remove vite

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -7,6 +7,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
  */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
 };
 
 export default withVanillaExtract(nextConfig);
